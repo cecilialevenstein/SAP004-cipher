2b6e77324b728ac08f9995ba521717906145cf13
Cecília Levenstein <c.helenalevenstein@gmail.com>
2020-04-12 00:18:45 -0300

    projeto finalizado

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,58 @@
 const cipher = {
-  // ...
+  encode: function(numero, texto) {
+    if (typeof texto != 'string') {
+      throw TypeError("texto undefined");
+    }
+    numero = parseInt(numero);
+    let resultado = "";
+    for(let contador = 0; contador < texto.length; contador++) {
+
+      let charcode = texto.charCodeAt(contador);
+      if (charcode > 122 || charcode < 65 || (charcode > 90 && charcode < 97)) {
+        // concatena o que nao for de "a-z A-Z"
+        resultado += texto[contador];
+        continue;
+      }
+
+      let min = 65;// A-Z maiusculo
+      if (charcode >= 97 && charcode <= 122) {
+        // a-z minusculo
+        min = 97;
+      }
+
+      let indexValido = (((texto.charCodeAt(contador) - min) + numero)%26 )+ min;
+      resultado += String.fromCharCode(indexValido);
+    }
+
+    return resultado;
+  },
+
+  decode: function(numero, texto) {
+  if (typeof texto!= 'string'){
+  throw TypeError("texto undefined");
+  }
+    numero = parseInt(numero);
+    let resultado = "";
+    for(let contador = 0; contador < texto.length; contador++){
+      
+      let charcode = texto.charCodeAt(contador);
+      if (charcode > 122 || charcode < 65 || (charcode > 90 && charcode < 97)) {
+        // concatena o que nao for de "a-z A-Z"
+        resultado += texto[contador];
+        continue;
+      }
+
+      let mai = 90
+      if (charcode >= 97 && charcode <= 122) {
+
+        mai = 122
+      }
+
+      let indexValido = (((texto.charCodeAt(contador) - mai) - numero)% 26) + mai;
+      resultado += String.fromCharCode(indexValido);
+    }
+    return resultado;
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//cipher.encode(1,"abc")
+document.getElementById("code").addEventListener("click", function(){
+    let ceaserNum = document.getElementById("ceaserNum").value;
+
+    let codeMessage = document.getElementById("codeMessage").value;
+
+    let resultado = cipher.encode(ceaserNum, codeMessage);
+
+    document.getElementById ("resultado").innerHTML = resultado;
+})
+
+document.getElementById("decode").addEventListener("click", function(){
+    let ceaserNum = document.getElementById("ceaserNum").value;
+
+    let codeMessage = document.getElementById("codeMessage").value;
+
+    let resultado = cipher.decode(ceaserNum, codeMessage);
+
+    document.getElementById ("resultado").innerHTML = resultado;
+})
